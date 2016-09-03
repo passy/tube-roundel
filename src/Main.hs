614e49a8d5c9e9ad473ce42e1031645ed2743512
Pascal Hartig <phartig@rdrei.net>
2016-09-03 20:30:02 +0100

    Properly validate color input

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
@@ -10,11 +11,10 @@ import Prelude ()
 import Prelude.Compat
 import Servant
 
-import Debug.Trace.Compat
-
 import Network.Wai (Application)
 import Control.Monad.IO.Class (liftIO)
 import Network.Wai.Handler.Warp (runEnv)
+import GHC.Generics (Generic)
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
@@ -23,22 +23,31 @@ import qualified Data.Text.IO as TIO
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TL
 import qualified Data.Text.Template as TT
+import qualified Data.Char as Char
+
+newtype ColorHex = ColorHex T.Text
+  deriving (Eq, Show, Generic)
+
+instance FromHttpApiData ColorHex where
+  parseUrlPiece param =
+    let digits = T.takeWhile Char.isHexDigit $ T.take 6 param
+    in if T.null digits
+      then Left "Invalid color provided. Only hex digits allowed."
+      else Right $ ColorHex $ T.toLower digits
 
 -- SVG/PNG Mimetypes?
-type API = "roundel" :> "no-text" :> Capture "color" T.Text :> Get '[OctetStream] BS.ByteString
+type API = "roundel" :> "no-text" :> Capture "color" ColorHex :> Get '[OctetStream] BS.ByteString
 
 api :: Proxy API
 api = Proxy
 
-roundelHandler :: T.Text -> Handler BS.ByteString
-roundelHandler color = do
-  -- TODO: Validate color. No joke, forget this and we're in big trouble, bro.
+roundelHandler :: ColorHex -> Handler BS.ByteString
+roundelHandler (ColorHex color) = do
   tmpl <- liftIO $ TIO.readFile "res/roundel-no-text.svg.tpl"
   -- TODO: Avoid partial function.
   let context "colorHex" = color
   -- TODO: Consider using templateSafe.
-  let res = BSL.toStrict . TL.encodeUtf8 $ TT.substitute tmpl context
-  return $ traceShowId res
+  return $ BSL.toStrict . TL.encodeUtf8 $ TT.substitute tmpl context
 
 server :: Server API
 server = roundelHandler
