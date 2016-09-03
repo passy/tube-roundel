af85db9289a49024589e3fe964ca5839ef2f26a6
Pascal Hartig <phartig@rdrei.net>
2016-09-03 20:06:59 +0100

    Serve the template for starters

--- src/Main.hs
+++ src/Main.hs
@@ -11,18 +11,26 @@ import Prelude.Compat
 import Servant
 
 import Network.Wai (Application)
+import Control.Monad.IO.Class (liftIO)
 import Network.Wai.Handler.Warp (runEnv)
+import qualified Data.ByteString as BS
 
-type StaticAPI = "static" :> Raw
+-- TODO: Text?
+type API = "roundel" :> Capture "filename" String :> Get '[OctetStream] BS.ByteString
 
-staticAPI :: Proxy StaticAPI
-staticAPI = Proxy
+api :: Proxy API
+api = Proxy
 
-server :: Server StaticAPI
-server = serveDirectory "res"
+roundelHandler :: String -> Handler BS.ByteString
+roundelHandler name = do
+  file <- liftIO $ BS.readFile "res/roundel-no-text.svg.tpl"
+  return file
+
+server :: Server API
+server = roundelHandler
 
 app :: Application
-app = serve staticAPI server
+app = serve api server
 
 main :: IO ()
 main = runEnv 8080 app
