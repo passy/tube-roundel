ff744a9bdf75e655b99f94b203ff9d5100552e39
Pascal Hartig <phartig@rdrei.net>
2016-09-03 20:08:01 +0100

    Always use Text

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ module Main where
 import Prelude ()
 import Prelude.Compat
 import Servant
+import qualified Data.Text as T
 
 import Network.Wai (Application)
 import Control.Monad.IO.Class (liftIO)
@@ -16,15 +17,14 @@ import Network.Wai.Handler.Warp (runEnv)
 import qualified Data.ByteString as BS
 
 -- TODO: Text?
-type API = "roundel" :> Capture "filename" String :> Get '[OctetStream] BS.ByteString
+type API = "roundel" :> Capture "filename" T.Text :> Get '[OctetStream] BS.ByteString
 
 api :: Proxy API
 api = Proxy
 
-roundelHandler :: String -> Handler BS.ByteString
-roundelHandler name = do
-  file <- liftIO $ BS.readFile "res/roundel-no-text.svg.tpl"
-  return file
+roundelHandler :: T.Text -> Handler BS.ByteString
+roundelHandler name =
+  liftIO $ BS.readFile "res/roundel-no-text.svg.tpl"
 
 server :: Server API
 server = roundelHandler
