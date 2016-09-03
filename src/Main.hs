0d2fba5e91d571b7bb1393396211a5f53f20f851
Pascal Hartig <phartig@rdrei.net>
2016-09-03 20:20:14 +0100

    Replace color in template

--- src/Main.hs
+++ src/Main.hs
@@ -9,22 +9,36 @@ module Main where
 import Prelude ()
 import Prelude.Compat
 import Servant
-import qualified Data.Text as T
+
+import Debug.Trace.Compat
 
 import Network.Wai (Application)
 import Control.Monad.IO.Class (liftIO)
 import Network.Wai.Handler.Warp (runEnv)
-import qualified Data.ByteString as BS
 
--- TODO: Text?
-type API = "roundel" :> Capture "filename" T.Text :> Get '[OctetStream] BS.ByteString
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
+import qualified Data.Text.Template as TT
+
+-- SVG/PNG Mimetypes?
+type API = "roundel" :> "no-text" :> Capture "color" T.Text :> Get '[OctetStream] BS.ByteString
 
 api :: Proxy API
 api = Proxy
 
 roundelHandler :: T.Text -> Handler BS.ByteString
-roundelHandler name =
-  liftIO $ BS.readFile "res/roundel-no-text.svg.tpl"
+roundelHandler color = do
+  -- TODO: Validate color. No joke, forget this and we're in big trouble, bro.
+  tmpl <- liftIO $ TIO.readFile "res/roundel-no-text.svg.tpl"
+  -- TODO: Avoid partial function.
+  let context "colorHex" = color
+  -- TODO: Consider using templateSafe.
+  let res = BSL.toStrict . TL.encodeUtf8 $ TT.substitute tmpl context
+  return $ traceShowId res
 
 server :: Server API
 server = roundelHandler
